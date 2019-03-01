eb4c6c976e044319c8082e99351daa3e9fabff3d
AdirthaBorgohain <adirthaborgohain@gmail.com>
2019-03-02 01:22:05 +0530

    Implemented k-means for euclidean distance

--- k-means.py
+++ k-means.py
@@ -2,7 +2,11 @@ import os
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from math import *
+from decimal import Decimal
 
+def euclidean_distance(point, center):
+    return np.sqrt(np.sum((point - center) ** 2))
 
 def plot_points(data_points):
     x, y = data_points.T
@@ -10,13 +14,18 @@ def plot_points(data_points):
     plt.title("Unclustered Data")
     plt.show
 
-
 def create_centers(k):
     centers = []
     for i in range(k):
         centers.append([random.randint(0, 100), random.randint(0, 100)])
     return np.array(centers)
 
+def compute_new_centers(cluster, centers):
+    return np.array(cluster + centers)/2
+
+def assign_cluster(distance, data_point, centers):
+    index_min = min(distance, key = distance.get)
+    return [index_min, data_point, centers[index_min]]
 
 def k_means(data_points, centers, p_measure):
     label = []
@@ -29,7 +38,7 @@ def k_means(data_points, centers, p_measure):
             distance = {}
             for center_index in range(0, k):
                 if(p_measure == 'm'):
-                    distance[center_index] = minkowski_distance(
+                    distance[center_index] = euclidean_distance(
                         data_points[index_point], centers[center_index])
                 elif(p_measure == 'p'):
                     distance[center_index] = pearson_correlation(
@@ -42,8 +51,14 @@ def k_means(data_points, centers, p_measure):
                 label[1], centers[label[0]])
             if epoch == (200 - 1):
                 cluster.append(label)
-        return [cluster, centers]
+    return [cluster, centers]
 
+def print_cluster_data(result):
+    print("Result of k-Means Clustering: \n")
+    for data in result[0]:
+        print("data point: {}".format(data[1]))
+        print("cluster number: {} \n".format(data[0]))
+    print("Last centroids position: \n {}".format(result[1]))
 
 if __name__ == "__main__":
     data_points = np.genfromtxt("assets/data.csv", delimiter=",")
@@ -61,3 +76,5 @@ if __name__ == "__main__":
     k = int(input("No. of clusters: "))
     centers = create_centers(k)
     [cluster, new_centers] = k_means(data_points, centers, p_measure)
+    print_cluster_data([cluster, new_centers])
+    print()
